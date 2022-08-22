26ae60f8d469723be5cbf137d71785cf2e8f84ef
David Jahn <david.jahn97@gmx.de>
2022-08-22 12:55:26 +0200

    Configuration Checked

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => {
+    options.AddPolicy(
+    "AllowAll", b => {
+    b.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.WriteTo.Console().ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
@@ -17,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
